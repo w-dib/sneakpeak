8cf7fbef98c15ea2caf7be8bff9b4c4a869f3fed
Walid Daniel Dib <112762655+w-dib@users.noreply.github.com>
2025-06-19 00:08:23 +0400

    update auth redirect on vercel builds

--- src/components/auth-form.tsx
+++ src/components/auth-form.tsx
@@ -21,16 +21,14 @@ export function AuthForm() {
   const [message, setMessage] = useState("");
 
   const getURL = () => {
-    let url =
-      process?.env?.NEXT_PUBLIC_SITE_URL ?? // Set this to your site URL in production env.
-      process?.env?.NEXT_PUBLIC_VERCEL_URL ?? // Automatically set by Vercel.
+    const url =
+      process.env.NEXT_PUBLIC_VERCEL_URL ||
+      process.env.NEXT_PUBLIC_SITE_URL ||
       "http://localhost:3000/";
-    // Make sure to include `https` in production URLs.
-    url = url.includes("http") ? url : `https://${url}`;
-    // Make sure to include a trailing `/`.
-    url = url.charAt(url.length - 1) === "/" ? url : `${url}/`;
-    url = `${url}auth/callback`;
-    return url;
+
+    const ansoluteUrl = url.startsWith("http") ? url : `https://${url}`;
+
+    return `${ansoluteUrl}/auth/callback`;
   };
 
   const signInWithGoogle = async () => {
